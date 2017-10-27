5d0864e69dc32b22e1f71f27e70c45a43ef6128e
Nupura <nupura.pantina@gmail.com>
2017-10-28 00:58:42 +1100

    deleting user

---
+++ userDeleteSuccess.php
@@ -0,0 +1,110 @@
+<?php
+if(isset($_COOKIE['uname']))
+{
+	$username = $_COOKIE['uname'];
+	$right = $_COOKIE['privilage'];
+?>
+<html>
+	<head>
+		<title>User Deleted</title>
+		<style type="text/css">
+			body{
+				background-color: lightgreen;
+				height: 100vh;
+				width: 100vh;
+				position: fixed;
+			}
+			#content{
+				height: 100vh;
+				left:32%;
+				width: 68%;
+				position: fixed;
+			}
+			#navMenu{
+				background-color: pink;
+				height: 100vh;
+				width:30%;
+				position: fixed;
+				top: 0%:
+			}
+			#searchTable{
+				background-color:yellow;
+			}
+			h3,h2{
+				color: darkblue;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="content">
+			<?php
+				$dbuser = "npantin"; 
+				$dbpass = "Mlniss#23"; 
+				$db = "SSID"; 
+				$connect = ocilogon($dbuser, $dbpass, $db);
+
+				if (!$connect) {
+					echo "An error occurred connecting to the database";
+					exit;
+				}
+			
+				$sql="DELETE FROM userlogin WHERE staff_id=$delete";
+				echo ("<h2>Successfully deleted</h2>");
+				echo ("<a href=deleteUser.php>Go Back</a>");
+
+				// check the sql statement for errors and if errors report them
+				$stmt = ociparse($connect, $sql);
+
+				if(!$stmt) {
+					echo "An error occurred in parsing the sql string.\n";
+					exit;
+				}
+				ociexecute($stmt);
+				ocilogoff ($connect);
+			?> 
+		</div>
+		
+		<div id="navMenu">
+			<button onclick='window.history.back() '> Go Back </button>
+			<h3>You are now logged in to the : <b><?echo $right;?></b> privilege account<h3></br>
+			<a href="index.php">LOGOUT</a></br></br>
+			<h3>MENU</h3></br>
+			<a href="viewStaffMembers.php">View Staff Members</a></br></br>
+			<?php
+				if( $right == admin)
+				{
+					echo ("<a href=addNewStaff.php>Add New Staff</a></br></br>");
+					echo ("<a href=addNewUser.php>Add New User to Database</a></br></br>");
+					echo ("<a href=displayUser.php>Display Users from Database</a></br></br>");
+					echo ("<a href=deleteUser.php>Delete User from Database</a></br></br>");
+				}
+			?>
+			<div id="searchTable">
+				<form name="formSearch" method="post" action="searchResult.php">
+					<h2>Search Staff</h2>
+					<table>
+						<tr>
+							<td>Surname:</td>
+							<td><input type="text" name="searchSurname" autocomplete="off"></td>
+						</tr>
+						<tr>
+							<td>Given Name:</td>
+							<td><input type="text" name="searchGivenName" autocomplete="off"></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type="submit" name="search" value="Search"></td>
+						</tr>
+					</table>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+<?php
+}
+else
+{
+	echo "invalid";
+}
+?>
